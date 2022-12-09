23805a1a4dc652bd04f7a25acb9575c551b980b8
Amanda Soares Oliveira <amanda.oliveira@dtidigital.com.br>
2022-12-09 00:16:54 -0300

    merge

--- semantic/SemanticAnalysis.cpp
+++ semantic/SemanticAnalysis.cpp
@@ -5,19 +5,55 @@
 
 #include "SemanticAnalysis.h"
 
+SymbolTable symboltable;
+
 SemanticAnalysis::SemanticAnalysis(SymbolTable* st) : symbolTable(st){
 }
+
 
 SemanticAnalysis::~SemanticAnalysis(){}
 
 bool SemanticAnalysis::equalTypes(Lexeme l1, Lexeme l2){
-	if(l1.type == l2.type){
-		return true;
-	}
-	return false;
+  if(l1.type == l2.type){
+    return true;
+  }
+  return false;
 }
 
-//essa aqui é pra pegar TIPO na TS
+bool SemanticAnalysis::varDeclared(std::string var){
+  if(symboltable.contains(var)){
+    return true;
+  }
+  return false;
+}
+
+// essa aqui é pra add valor na TS
+void SemanticAnalysis:: appendValueST(std::string var, std::string value){
+  symboltable.m_st.insert(value, symboltable.find(var));
+}
+
+//essa aqui é pra pegar Valor na TS
+std:: string getIdValueST(std::string var){
+  std::string lex = symboltable.find(var);
+  return lex.value;
+}
+/* 
+    node_type extract(const key_type& _Keyval) {
+        const const_iterator _Where = find(_Keyval);
+        if (_Where == end()) {
+            return node_type{};
+        }
+
+        return extract(_Where);
+    }*/
+
+
+// essa aqui é pra add tipo na TS
+void SemanticAnalysis:: appendTypeST(std::string var, int type){
+symboltable.m_st.insert(type, symboltable.find(var));
+}
+
+//essa aqui é pra pegar Valor na TS
 int getIdTypeST(std::string var){
   return symboltable.find(var);
 }
